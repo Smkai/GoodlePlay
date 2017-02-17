1d65882d07a20d00a2329f3aaa2f3c1e68d2bb7f
Administrator <35600>
2017-02-17 10:54:17 +0800

    加载更多listviewFragment基类的创建

--- app/src/main/java/ck/itheima/com/goodleplay/app/Constans.java
+++ app/src/main/java/ck/itheima/com/goodleplay/app/Constans.java
@@ -10,7 +10,7 @@ package ck.itheima.com.goodleplay.app;
  */
 
 public class Constans {
-    public static final String BASE_URL = "http://192.168.18.84:8080/GooglePlayServer/";
+    public static final String BASE_URL = "http://192.168.76.1:8080/GooglePlayServer/";
     public static final String BASE_IMAGEUR = BASE_URL + "image?name=";
 
 }
---
+++ app/src/main/java/ck/itheima/com/goodleplay/base/BaseLoadmoreListFragment.java
@@ -0,0 +1,39 @@
+package ck.itheima.com.goodleplay.base;
+
+import android.widget.AbsListView;
+
+import ck.itheima.com.goodleplay.fragment.BaseListFragment;
+
+/**
+ * 类名:    BaseLoadmoreListFragment
+ * 创建者:  ckqu
+ * 创建时间:2017/2/17 0017 上午 10:44
+ * 包名:    ck.itheima.com.goodleplay.base
+ * 更新者:  $Author$ $Date$
+ * 描述:    带有下拉刷新功能的fragment
+ */
+
+public abstract class BaseLoadmoreListFragment extends BaseListFragment {
+    @Override
+    protected void initListView() {
+        super.initListView();
+        getListView().setOnScrollListener(mOnScrollListener);
+    }
+
+    AbsListView.OnScrollListener mOnScrollListener = new AbsListView.OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+            if (getListView().getLastVisiblePosition() == getAdapter().getCount() - 1){
+                starLoadMoreData();//加载更多数据 让子类自行完成
+            }
+
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+        }
+    };
+
+    protected abstract void starLoadMoreData();
+}
